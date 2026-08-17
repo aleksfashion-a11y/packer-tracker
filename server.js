23f231bc5959e0666bf8a84a9df772b690712587
aleksfashion-a11y <aleks.fashion@mail.ru>
2026-08-17 17:10:02 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const os = require("os");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DATA_DIR = path.join(__dirname, "data");
+const DATA_DIR = process.env.DATA_DIR || path.join(os.tmpdir(), "packer-tracker-data");
 const DATA_FILE = path.join(DATA_DIR, "data.json");
 
 if (!fs.existsSync(DATA_DIR)) fs.mkdirSync(DATA_DIR, { recursive: true });
