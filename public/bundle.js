782e448c8464f475cce221a2bb273346c7c16c26
aleksfashion-a11y <aleks.fashion@mail.ru>
2026-08-20 15:58:01 +0300

    Add files via upload

--- public/bundle.js
+++ public/bundle.js
@@ -80455,7 +80455,10 @@ Take a look at the reducer(s) handling this action type: ${action.type}.
       writeFileSync(wb, `payroll_${todayStr()}.xlsx`);
     };
     const removeUser = async (id) => {
-      await persistUsers(users.filter((u) => u.id !== id));
+      const u = users.find((x2) => x2.id === id);
+      askConfirm(`\u0423\u0434\u0430\u043B\u0438\u0442\u044C \u0441\u043E\u0442\u0440\u0443\u0434\u043D\u0438\u043A\u0430 \xAB${u ? u.name : "?"}\xBB? \u0415\u0433\u043E \u043B\u043E\u0433\u0438\u043D \u0438 \u043F\u0430\u0440\u043E\u043B\u044C \u0431\u0443\u0434\u0443\u0442 \u0443\u0434\u0430\u043B\u0435\u043D\u044B \u0431\u0435\u0437\u0432\u043E\u0437\u0432\u0440\u0430\u0442\u043D\u043E (\u0441\u0430\u043C\u0438 \u0437\u0430\u043F\u0438\u0441\u0438 \u0443\u043F\u0430\u043A\u043E\u0432\u043A\u0438 \u043E\u0441\u0442\u0430\u043D\u0443\u0442\u0441\u044F \u0432 \u0436\u0443\u0440\u043D\u0430\u043B\u0435).`, async () => {
+        await persistUsers(users.filter((x2) => x2.id !== id));
+      });
     };
     const saveRate = async (id) => {
       const rate = parseFloat(editRateVal.replace(",", ".")) || 0;
